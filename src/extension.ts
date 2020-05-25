b82c9bd0d98c75079341cedf13932b5bf5d011c7
Wuelber Castillo <wuelber.castillo@gmail.com>
2020-05-24 21:13:11 -0600

    Remove completion provider from plugin 🚽

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,12 @@
 import { languages, ExtensionContext } from "vscode";
 import VoxaStatesLinkProvider from "./VoxaStatesLinkProvider";
-import VoxaStatesCompletionItemProvider from "./VoxaStatesCompletionItemProvider";
 
 export function activate(context: ExtensionContext) {
   const linkProvider = languages.registerDocumentLinkProvider(
     { scheme: "file", pattern: "**/states/**/*.{ts,js}" },
     new VoxaStatesLinkProvider(),
   );
-  const completionItemProvider = languages.registerCompletionItemProvider(
-    { scheme: "file", pattern: "**/states/**/*.{ts,js}" },
-    new VoxaStatesCompletionItemProvider(),
-    ...['"', "'"],
-  );
   context.subscriptions.push(linkProvider);
-  context.subscriptions.push(completionItemProvider);
 }
 
 export function deactivate() {}
--- src/regex.ts
+++ src/regex.ts
@@ -2,4 +2,3 @@ export const onState = /onState[(]['"][^"]+['"]/g;
 export const toState = /to\s?[:=]\s['"][\w\W]+['"]/g;
 export const quotationMarks = /["']/g;
 export const spacesTabsAndLineBreaks = /\s{2,}|\t|\r\n|\n|\r|/g;
-export const toKeywordWithoutState = /to\s?[:=]\s['"]['"]?/g;
